cea8fb4d2eeb26e429622efe8e5bf07bad17b78a
Jangseon-Park <jap036@ucsd.edu>
2024-03-20 09:00:36 -0700

    <Function>[simple_bert.py] Add the simple_bert.py
    Description
    - Just add the simple bert python file

---
+++ bert/simple_bert.py
@@ -0,0 +1,44 @@
+from transformers import BertTokenizer, BertModel
+import torch
+from torch.profiler import ExecutionTraceObserver, profile
+
+def trace_hanelr(prof):
+    prof.export_chrome_trace("kineto_trace.json")
+
+# GPU 사용 가능 여부 확인
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+# BERT 토크나이저 및 모델 초기화, 모델을 GPU로 이동
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-base-uncased').to(device)
+
+# 토크나이징
+input_text = "Hello, my dog is cute"
+tokens = tokenizer(input_text, return_tensors="pt", padding=True, truncation=True)
+
+# 토큰 텐서를 GPU로 이동
+tokens = {k: v.to(device) for k, v in tokens.items()}
+
+# 프로파일링을 위한 콜백 함수 정의
+et = ExecutionTraceObserver()
+et.register_callback("pytorch_et.json")
+with profile(
+    activities=[
+        torch.profiler.ProfilerActivity.CPU,
+        torch.profiler.ProfilerActivity.CUDA,
+    ],
+    schedule=torch.profiler.schedule(wait=0, warmup=10, active=1),
+    on_trace_ready=trace_hanelr
+) as prof:
+    # 특징 추출
+    et.start()
+    with torch.no_grad():
+        outputs = model(**tokens)
+        last_hidden_states = outputs.last_hidden_state
+    et.stop()
+
+et.unregister_callback()
+# `last_hidden_states`는 모델의 마지막 레이어에서의 특징을 담고 있습니다.
+# `.cpu()`를 호출하여 CPU로 데이터를 이동시키고 `.numpy()`를 호출하여 numpy 배열로 변환할 수 있습니다.
+print(last_hidden_states.shape)  # (batch_size, sequence_length, hidden_size)
